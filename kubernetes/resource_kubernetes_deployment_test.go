c3c0ae90b00246621cfa7f8fb4bddd2dc44b1e45
Raul Macian <rmacian@full-on-net.com>
2019-04-02 15:57:51 +0200

    [limit] replicas for test to 1

--- kubernetes/resource_kubernetes_deployment_test.go
+++ kubernetes/resource_kubernetes_deployment_test.go
@@ -1235,7 +1235,7 @@ resource "kubernetes_deployment" "test" {
   }
 
   spec {
-    replicas = 100 # This is intentionally high to exercise the waiter
+    replicas = 1 
 
     selector {
       match_labels {
